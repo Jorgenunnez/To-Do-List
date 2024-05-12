2fc1d7ccd63565a211c751af5bb8078ef6ad3838
jorge <145932929+Jorgenunnez@users.noreply.github.com>
2024-05-12 18:14:03 -0300

    Add files via upload
    cambios al javascript

--- script.js
+++ script.js
@@ -2,23 +2,55 @@ const fecha = document.querySelector("#fecha");
 const lista = document.querySelector("#lista");
 const input = document.querySelector("#input");
 const botonenter = document.querySelector("#enter");
+const check = 'fi fi-br-check';
+const uncheck= 'fi fi-bs-circle';
+const lineThrough= 'line-through';
+let id = 0;
 
 //funcion agregar tarea
 
-function agregarTarea(tarea) {
+function agregarTarea(tarea,id,Realizado,Eliminado) {
+
+    if(Eliminado){return}
+
+    const REALIZADO = Realizado ?check :uncheck
+    const LINE = Realizado ?lineThrough :''
+
   const elemento = ` <li id="elemento">    
-  <i class="fi fi-bs-circle" data="Realizado" id="0"></i>
-  <p class="text">${tarea}</p>
-  <i class="fi fi-rr-trash-xmark" data="Eliminado" id="0"></i>
+  <i class="fi ${REALIZADO}" data="Realizado" id="${id}"></i>
+  <p class="text ${LINE}">${tarea}</p>
+  <i class="fi fi-rr-trash-xmark" data="Eliminado" id="${id}"></i>
 </li>`
 
     lista.insertAdjacentHTML("beforeend",elemento)
 }
-
+// EVENTO DE AGREGAR LA TAREA MEDIANTE CLICK
 botonenter.addEventListener('click',()=>{
     const tarea= input.value
     if(tarea){
-        agregarTarea(tarea)
+        agregarTarea(tarea,id,false,false)
     }
     input.value=''
+    id++
 });
+
+// EVENTO DE AGREGAR LA TAREA CON EL BOTON ENTER
+
+document.addEventListener('keyup', function(event){
+    if(event.key == 'Enter'){
+        const tarea= input.value
+        if(tarea){
+            agregarTarea(tarea,id,false,false)
+        }
+        input.value=''
+        id++
+    }
+});
+
+
+//EVENTO PARA CHECK DE TAREAS 
+
+lista.addEventListener('click',function(event){
+    const element= event.target
+    const elementData= element.attributes.data.value
+})
